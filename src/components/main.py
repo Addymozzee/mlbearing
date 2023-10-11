a67ee5b287bff5fb32114339b39aadaf5b68fd44
Addymozzee <addymozzee@gmail.com>
2023-10-11 02:38:24 +0100

    mlbearing commit

--- src/components/main.py
+++ src/components/main.py
@@ -3,7 +3,7 @@ from src.components.model_training import ModelTrainer
 
 if __name__ == "__main__":
     data_transformation = DataTransformation()
-    directory_path = 'C:/Users/Aeesha/DissProject/mlbearing/notebook/bearing/1st_test/1st_test'
+    directory_path = 'C:/Users/tmade/DissProject/mlbearing/notebook/bearing/1st_test/1st_test'
     
     ct = data_transformation.initiate_data_transformation(directory_path)
     cb = ModelTrainer() # Initialize and possibly use the ModelTrainer here as necessary
